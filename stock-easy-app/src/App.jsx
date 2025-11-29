e6f8aee359da63d9b313d94741707c3db3d94cd3
silver-bandicoot-dev <jory.cherief@gmail.com>
2025-11-29 12:14:06 +0100

    fix: ajouter extension .jsx à l'import Stockeasy pour compatibilité Vercel

--- stock-easy-app/src/App.jsx
+++ stock-easy-app/src/App.jsx
@@ -9,7 +9,7 @@ import SupabaseResetPassword from './components/auth/SupabaseResetPassword';
 import AcceptInvitation from './components/auth/AcceptInvitation';
 import EmailConfirmation from './components/auth/EmailConfirmation';
 import NotificationsPage from './components/notifications/NotificationsPage';
-import Stockeasy from './Stockeasy';
+import Stockeasy from './Stockeasy.jsx';
 import ProfileRedirect from './components/profile/ProfileRedirect';
 import SupabaseConnectionTest from './components/debug/SupabaseConnectionTest';
 import Landing from './pages/Landing';
